28edf2d5eddbaf313a3f20336ceb02f310d6f279
Nikolai Gusarov <nikolai_polos@mail.ru>
2025-07-17 14:16:49 +0300

    авторизация покуматель-почта, менеджер-username2

--- store_app/forms/auth_forms.py
+++ store_app/forms/auth_forms.py
@@ -21,7 +21,7 @@ class CustomerSignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('email', 'first_name', 'last_name', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=False)
