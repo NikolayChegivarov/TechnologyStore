d97ed052f218078a549b4e6cca6125cebd95af21
Nikolai Gusarov <nikolai_polos@mail.ru>
2025-09-04 14:46:49 +0300

    корректировка settings под продакшон2

--- store_project/settings.py
+++ store_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dj6u$!i+++i7#-ui2wsldrq#8l(f2!&e5q892k1s7#m6sy*g7+')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
