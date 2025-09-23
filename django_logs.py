878a788f3a771695e60c525120802d2cd07f2dbd
Николай <nikolai_polos@mail.ru>
2025-09-23 05:50:01 +0000

    staticfiles

---
+++ django_logs.py
@@ -0,0 +1,19 @@
+# Логирование
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/gadgetia/TechnologyStore/django_errors.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
