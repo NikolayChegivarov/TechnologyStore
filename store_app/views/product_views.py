d2efd1d7facc4439a82e8bdbc237906c9953be7c
Nikolai Gusarov <nikolai_polos@mail.ru>
2025-09-24 08:59:57 +0300

    убрал лишнее главное/панель менеджера

--- store_app/views/product_views.py
+++ store_app/views/product_views.py
@@ -102,31 +102,6 @@ def deactivate_products(request):
     return redirect('manager_dashboard')
 
 
-# def product_list(request, category_slug=None):
-#     """Отображает список товаров с возможностью фильтрации по категории:
-#     - Получает все доступные товары (available=True)
-#     - Если передан category_slug, фильтрует товары по категории
-#     - Возвращает список всех категорий для навигации
-#     - Использует шаблон product/list.html
-#
-#     Args:
-#         category_slug (str, optional): Слаг категории для фильтрации товаров
-#     """
-#     category = None
-#     categories = Category.objects.all()
-#     products = Product.objects.filter(available=True)
-#
-#     if category_slug:
-#         category = get_object_or_404(Category, slug=category_slug)
-#         products = products.filter(category=category)
-#
-#     return render(request, 'product/list.html', {
-#         'category': category,
-#         'categories': categories,
-#         'products': products
-#     })
-
-
 def product_detail(request, id, slug):
     """Отображает детальную страницу товара:
     - Находит товар по id и slug (двойная проверка URL)
